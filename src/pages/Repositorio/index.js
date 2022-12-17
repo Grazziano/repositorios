d8b7df4d99c6e2db10b1322b0097b33c439e5934
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-12-17 14:13:29 -0300

    Exibindo issues

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BackButton, Container, Loading, Owner } from './styles';
+import { BackButton, Container, IssuesList, Loading, Owner } from './styles';
 import api from '../../services/api';
 import { useParams } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
@@ -47,11 +47,32 @@ export default function Repositorio() {
       <BackButton to="/">
         <FaArrowLeft color="#000" size={30} />
       </BackButton>
+
       <Owner>
         <img src={repositorio.owner.avatar_url} alt={repositorio.owner.login} />
         <h1>{repositorio.name}</h1>
         <p>{repositorio.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   );
 }
